55a89ddeefac9b8b608d66935ddf137a527bb8cf
jpwakugawa <jp_wakugawa@outlook.com>
2022-06-12 12:40:27 -0400

    level1: echo

---
+++ levels/level1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo -n "LESSON 1 - ECHO
+
+To print something on terminal you must use 'echo'
+Example:
+	echo I love pizza
+
+Now, try yourself: "
+
+read answer
+
+echo "Nice job! Here is your output:"
+$answer
