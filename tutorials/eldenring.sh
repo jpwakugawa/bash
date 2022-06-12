109a03bdaf10cabd4e35845aca08a847337ef792
jpwakugawa <jp_wakugawa@outlook.com>
2022-06-12 12:20:39 -0400

    case esac

--- tutorials/eldenring.sh
+++ tutorials/eldenring.sh
@@ -3,6 +3,30 @@
 # This script came from NetworkChuck
 # https://www.youtube.com/watch?v=Fq6gqi9Ubog&t=320s
 
+echo "Welcome tarnished. Please select your starting class:
+1 - Samurai
+2 - Prisoner
+3 - Phophet"
+
+read class
+
+case $class in
+	1)
+		type="Samurai"
+		hp=10
+		attack=20;;
+	2)
+		type="Prisoner"
+		hp=20
+		attack=4;;
+	3)
+		type="Prophet"
+		hp=30
+		attack=4;;
+esac
+
+echo "You chosen the $type class. Your HP is $hp and yout attack is $attack."
+
 echo "You Died"
 
 # First beast battle
