87d2e05b2a59d00c1ad55b750ec0505ac45716aa
jpwakugawa <jp_wakugawa@outlook.com>
2022-10-06 09:05:09 -0400

    lua: variable declaration

---
+++ lua/variableDeclaration.lua
@@ -0,0 +1,20 @@
+#!/usr/local/bin/lua
+
+-- Variable definition
+local a, b
+
+-- Initialization
+a = 10
+b = 30
+
+print("Value of a:", a)
+print("Value of b:", b)
+
+-- Swapping of variables
+b, a = a, b
+
+print("Value of a:", a)
+print("Value of b:", b)
+
+f = 70.0/3.0
+print("Value of f:", f)
